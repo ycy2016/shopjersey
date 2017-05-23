a0fddafaf7971b5a49c10ae1d781aff2ec00ad00
ahycy2017 <ahycy2011@163.com>
2017-05-23 14:06:42 +0800

    pom.xml添加jar包

--- shopbyjersey/src/main/java/com/cn/controller/Rest.java
+++ shopbyjersey/src/main/java/com/cn/controller/Rest.java
@@ -5,20 +5,15 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
-import org.junit.experimental.theories.FromDataPoints;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import com.cn.enity.Client;
 
 @Component
 @Path("/usertest")
